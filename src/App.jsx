06268885c009fd39991778acb8f87031abf56365
alisonrobichaux <alisonrobichaux@gmail.com>
2024-02-23 14:51:27 -0600

    added footer and css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
 import Blue from "./components/Blue"
 import Red from "./components/Red"
+import Yellow from "./components/Yellow"
 import Home from "./components/Home"
 
 function App() {
@@ -9,20 +10,29 @@ function App() {
   return (
     <>
       <div id="container">
-        <h1>Hello React Router!</h1>
+        <h2>Hello, React Router!</h2>
         <div id="navbar">
           <Link to="/blue">Blue</Link>
           <Link to="/red">Red</Link>
+          <Link to="/yellow">Yellow</Link>
           <Link to="/">Home</Link>
         </div>
         <div id="main-section">
           <Routes>
             <Route path="/blue" element={<Blue/>} />
             <Route path="/red" element={<Red/>} />
+            <Route path="/yellow" element={<Yellow/>} />
             <Route path="/" element={<Home/>} />
           </Routes>
         </div>
-        
+        <footer>
+        <div id="navbar">
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+          <Link to="/yellow">Yellow</Link>
+          <Link to="/">Home</Link>
+        </div>
+        </footer>
       </div>
     </>
   )
